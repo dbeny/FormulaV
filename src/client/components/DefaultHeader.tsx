546f32a2127ab8225a17bd5a8364b4523fe15066
dbeny <durjan.benedek@proton.me>
2024-12-21 01:05:36 +0100

    white logo

--- src/client/components/DefaultHeader.tsx
+++ src/client/components/DefaultHeader.tsx
@@ -1,14 +1,14 @@
 import "../styles/DefaultHeader.scss";
 import React from "react";
 
-import outlineLogo from "../static/assets/logo/fv_outline.png";
+import whiteLogo from "../static/assets/logo/fv_white.png";
 import TextButton from "./TextButton";
 
 //todo implement parameters for future header contents (profile, etc)
 export default function DefaultHeader() {
 	return (
 		<div className={"defaultHeaderContainer"}>
-			<img src={outlineLogo} alt={""}/>
+			<img src={whiteLogo} alt={""}/>
 			<div className={"flex-1"}></div>
 			<div className={"defaultHeaderButtons"}>
 				<TextButton text={"Test"} clicked={() => {
